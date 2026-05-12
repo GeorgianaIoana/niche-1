392fba834d45eb1e465d31c0ec985fab4edd369f
Georgiana Stanciu <georgiana17stanciu@gmail.com>
2026-05-12 22:17:57 +0300

    Use migrate:fresh to fix   corrupted db state

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,8 +9,8 @@ php artisan config:clear
 php artisan route:clear
 php artisan view:clear
 
-# Run migrations
-php artisan migrate --force
+# Run migrations (fresh to reset bad state)
+php artisan migrate:fresh --force
 
 # Start the server
 exec php -S 0.0.0.0:3000 -t public
